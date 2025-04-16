587a72cabd534e98a9b957096c39b0f61998258b
vladi <aizisunanda@gmail.com>
2025-04-16 16:26:37 +0530

    Added keymap to toggle open and close floating Neogit in config/keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,28 @@
-vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', { noremap = true, silent = true, desc = 'Toggle NeoTree' })
-vim.keymap.set('n', '<leader>c', '<cmd>bd<cr>', {
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { noremap = true, silent = true, desc = "Toggle NeoTree" })
+vim.keymap.set("n", "<leader>c", "<cmd>bd<cr>", {
     noremap = true,
     silent = true,
     nowait = true,
-    desc = 'Close buffer'
+    desc = "Close buffer",
 })
-vim.keymap.set('i', '<C-BS>', '<C-w>', { noremap = true })
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
 
 --vim.keymap.del("n", "c") -- Normal mode
 --vim.keymap.del("i", "c") -- Insert mode
 --vim.keymap.del("v", "c") -- Visual mode
 -- In your keymaps.lua or directly in the Comment.nvim config
-vim.api.nvim_set_keymap('n', '<leader>/', '<Plug>(comment_toggle_linewise_current)',
-    { noremap = true, silent = true, desc = 'Toggle comment' })
-vim.api.nvim_set_keymap('v', '<leader>/', '<Plug>(comment_toggle_linewise_visual)',
-    { noremap = true, silent = true, desc = 'Toggle comment' });
+vim.api.nvim_set_keymap(
+    "n",
+    "<leader>/",
+    "<Plug>(comment_toggle_linewise_current)",
+    { noremap = true, silent = true, desc = "Toggle comment" }
+)
+vim.api.nvim_set_keymap(
+    "v",
+    "<leader>/",
+    "<Plug>(comment_toggle_linewise_visual)",
+    { noremap = true, silent = true, desc = "Toggle comment" }
+)
 
 -- Remap <LocalLeader>c-c to <LocalLeader>c-v for Vlime lisp server
 -- vim.api.nvim_set_keymap('n', '<LocalLeader>lv', '<LocalLeader>cc',
@@ -23,17 +31,44 @@ vim.api.nvim_set_keymap('v', '<leader>/', '<Plug>(comment_toggle_linewise_visual
 -- Unmap keys in normal mode
 local default_opts = { noremap = true, silent = true }
 
+local function toggle_neogit()
+    -- Check if a Neogit buffer is open
+    local neogit_open = false
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
+            local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
+            if filetype == "NeogitStatus" then
+                neogit_open = true
+                -- Get the window displaying this buffer
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    if vim.api.nvim_win_get_buf(win) == buf then
+                        -- Close the window
+                        vim.api.nvim_win_close(win, true)
+                        return
+                    end
+                end
+            end
+        end
+    end
+
+    -- If no Neogit buffer is open, open Neogit in floating mode
+    if not neogit_open then
+        vim.cmd("Neogit kind=floating")
+    end
+end
+
 local normal_keymaps = {
-    ['<A-Up>'] = ':m .-2<CR>==',
-    ['<A-Down>'] = ":m .+1<CR>==",
+    ["<A-Up>"] = ":m .-2<CR>==",
+    ["<A-Down>"] = ":m .+1<CR>==",
     ["<A-Left>"] = "<C-w>h",
     ["<A-Right>"] = "<C-w>l",
-    ['<C-s>'] = ':w<CR>',
-    [']b'] = ':bnext<CR>',
-    ['[b'] = ':bprevious<CR>',
+    ["<C-s>"] = ":w<CR>",
+    ["]b"] = ":bnext<CR>",
+    ["[b"] = ":bprevious<CR>",
     -- ['<C-h>'] = '<C-w>h',
     -- ['<C-l>'] = '<C-w>l',
-    ['<leader>h'] = ':noh<CR>',
+    ["<leader>h"] = ":noh<CR>",
+    ["<C-/>"] = toggle_neogit,
 }
 
 local insert_keymaps = {
@@ -45,7 +80,7 @@ local insert_keymaps = {
 }
 
 local visual_keymaps = {
-    ['<C-s>'] = 'Esc:w<CR>',
+    ["<C-s>"] = "Esc:w<CR>",
 }
 
 local function set_keymaps(mode_table, mode)
@@ -54,6 +89,6 @@ local function set_keymaps(mode_table, mode)
     end
 end
 
-set_keymaps(normal_keymaps, 'n')
-set_keymaps(insert_keymaps, 'i')
-set_keymaps(visual_keymaps, 'v')
+set_keymaps(normal_keymaps, "n")
+set_keymaps(insert_keymaps, "i")
+set_keymaps(visual_keymaps, "v")
