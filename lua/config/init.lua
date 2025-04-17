31f9deb58d4b1c0d4dfd19437ad8a34a291d1f9d
vladi <aizisunanda@gmail.com>
2025-04-17 14:44:57 +0530

    nothing really changed just committing staged change

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -23,13 +23,13 @@ local lsp_files = vim.fn.glob(config_path .. "/lua/config/lsps/*.lua", 0, 1)
 -- end
 
 local function get_module_name(file_path)
-	local module_name = file_path:gsub(config_path .. "/lua/", "")
-	module_name = module_name:gsub("/", ".")
-	module_name = module_name:gsub("%.lua$", "")
-	return module_name
+    local module_name = file_path:gsub(config_path .. "/lua/", "")
+    module_name = module_name:gsub("/", ".")
+    module_name = module_name:gsub("%.lua$", "")
+    return module_name
 end
 
 for _, file in ipairs(lsp_files) do
-	local module_name = get_module_name(file)
-	require(module_name)
+    local module_name = get_module_name(file)
+    require(module_name)
 end
