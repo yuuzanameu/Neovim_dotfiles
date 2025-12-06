10544ce7b065571611c9882f433e92ccbbc50221
vladi <aizisunanda@gmail.com>
2025-12-06 16:52:58 +0530

    new changes

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,83 +1,90 @@
 return {
-	"mfussenegger/nvim-dap",
-	lazy = true,
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"theHamsta/nvim-dap-virtual-text",
-	},
-	keys = {
-		{
-			"<leader>d",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Toggle breakpoint",
-		},
-		{
-			"<leader>g",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Continue debugging",
-		},
-		{
-			"<leader>,",
-			function()
-				require("dapui").eval(nil, { enter = true })
-			end,
-		},
-	},
-	config = function()
-		local dap = require("dap")
-		dap.adapters["pwa-node"] = {
-			type = "server",
-			host = "localhost",
-			port = "${port}",
-			executable = {
-				command = "node",
-				args = {
-					-- require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-					-- 	.. "js-debug/src/dapDebugServer.js",
-					"/home/vladi/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-					"${port}",
-				},
-			},
-		}
+  "mfussenegger/nvim-dap",
+  lazy = true,
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "theHamsta/nvim-dap-virtual-text",
+  },
+  keys = {
+    {
+      "<leader>d",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle breakpoint",
+    },
+    {
+      "<leader>g",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Continue debugging",
+    },
+    {
+      "<leader>,",
+      function()
+        require("dapui").eval(nil, { enter = true })
+      end,
+    },
+  },
+  config = function()
+    local dap = require("dap")
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = {
+          "/home/vladi/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+    }
 
-		dap.configurations.javascript = {
-			{
-				type = "pwa-node",
-				request = "launch",
-				name = "Launch file",
-				program = "${file}",
-				cwd = "${workspaceFolder}",
-			},
-		}
+    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+    local vscode = require("dap.ext.vscode")
+    vscode.type_to_filetypes["node"] = js_filetypes
+    vscode.type_to_filetypes["pwa-node"] = js_filetypes
 
-		dap.adapters.chrome = {
-			type = "executable",
-			command = "node",
-			args = {
-				-- require("mason-registry").get_package("chrome-debug-adapter"):get_install_path()
-				-- 	.. "/out/src/chromeDebug.js",
-				"/home/vladi/.local/share/nvim/mason/packages/chrome-debug-adapter/out/src/chromeDebug.js",
-			},
-		}
+    for _, language in ipairs(js_filetypes) do
+      if not dap.configurations[language] then
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end
 
-		dap.configurations.typescriptreact = {
-			{
-				type = "chrome",
-				request = "launch",
-				program = "${file}",
-				cwd = vim.fn.getcwd(),
-				sourceMaps = true,
-				protocol = "inspector",
-				port = 9222,
-				webRoot = "${workspaceFolder}",
-			},
-		}
-
-		require("dapui").setup()
-	end,
+    local setup = {
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+      controls = {
+        icons = {
+          pause = "▷",
+          play = "▶",
+          step_into = "▷",
+          step_over = "▶",
+          step_out = "▷",
+          step_back = "▶",
+          run_last = "▶",
+          terminate = "▷",
+          disconnect = "▷",
+        },
+      }
+    }
+    require("dapui").setup()
+  end,
 }
