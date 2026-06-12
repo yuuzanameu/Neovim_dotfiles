5ba754ce12354ba38a1f53a5e94b57df0d4c3080
vladi <aizisunanda@gmail.com>
2026-06-12 09:20:09 +0530

    telescope and tree-sitter updated to master and main respectively

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require('telescope').setup {
-            pickers = {
-                colorscheme = {
-                    enable_preview = true
-                }
-            }
-        }
-    end
+  "nvim-telescope/telescope.nvim",
+  -- tag = '0.1.8',
+  branch = "master",
+  dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+  config = function()
+    require("telescope").setup({
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+    })
+  end,
 }
