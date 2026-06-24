a800f68ac930bb0b1d5ec32e92ba65504b9893f5
vladi <aizisunanda@gmail.com>
2026-06-24 23:15:08 +0530

    cactus

--- lua/plugins/snack_dashboard.lua
+++ lua/plugins/snack_dashboard.lua
@@ -21,39 +21,68 @@ local shinchan = [[⠀
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⡘⠤⡁⠀⢡⠘⡠⢁⠣⡐⢡⠘⡰⢀⠎⠄⢣⠘⢠⠃⠔⡡⠘⡄⢊⠤⠑⡨⢐⠡⠒⡠⠀⣿⣿
 ]]
 
+local gif = "cactus"
+
 return {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-        dashboard = {
-            autokeys = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            preset = {
-                header = shinchan,
-            },
-            sections = {
-                {
-                    section = "header",
-                    padding = 1,
-                },
-                {
-                    pane = 2,
-                    {
-                        icon = " ",
-                        title = "Recent Projects",
-                        section = "projects",
-                        indent = 2,
-                        padding = 1,
-                    },
-                    {
-                        icon = " ",
-                        title = "Recent Files",
-                        section = "recent_files",
-                        indent = 2,
-                        padding = 1,
-                    },
-                },
-                { section = "startup" },
-            },
-        },
-    },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	dependencies = { "amansingh-afk/milli.nvim" },
+	opts = function()
+		local splash = require("milli").load({ splash = gif })
+		return {
+			dashboard = {
+				enabled = true,
+				preset = {
+					header = table.concat(splash.frames[1], "\n"),
+				},
+				sections = {
+					{ section = "header", padding = 0 },
+					-- { section = "keys",   gap = 1,    padding = 1 },
+					-- { section = "startup" },
+				},
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("snacks").setup(opts)
+		require("milli").snacks({ splash = gif, loop = true })
+	end,
 }
+
+-- local M = {
+--     "folke/snacks.nvim",
+--     ---@type snacks.Config
+--     opts = {
+--         dashboard = {
+--             autokeys = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+--             preset = {
+--                 header = shinchan,
+--             },
+--             sections = {
+--                 {
+--                     section = "header",
+--                     padding = 1,
+--                 },
+--                 {
+--                     pane = 2,
+--                     {
+--                         icon = " ",
+--                         title = "Recent Projects",
+--                         section = "projects",
+--                         indent = 2,
+--                         padding = 1,
+--                     },
+--                     {
+--                         icon = " ",
+--                         title = "Recent Files",
+--                         section = "recent_files",
+--                         indent = 2,
+--                         padding = 1,
+--                     },
+--                 },
+--                 { section = "startup" },
+--             },
+--         },
+--     },
+-- }
